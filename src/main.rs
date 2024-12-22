b8e2bd0161bb61e01e2adac94da1681cdfef0796
Tushka1 <tushka20122008@gmail.com>
2024-12-22 12:04:03 +0300

    basic TCP server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            stream.write(&buffer[0..size]).unwrap();
+            true
+        }
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
+            false
+        }
+    } {
+        // code
+    }
 }
+
+pub fn main() {
+    let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
+    println!("Server listening on port 25565");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
+
